b1b25f67a80f1b299c9e0c2c55cc83243fe1e943
radoslawstepniewski <radoslawstepniewski89@gmail.com>
2018-08-02 22:55:35 +0200

    added constructor function

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ let customer = {
     }
 }
 document.write(customer.speak() + "<br />")
-document.write(customer.name + "lives" + customer.adress.street);
+document.write(customer.name + "lives" + customer.adress.street + "<br />");
 customer.adress.coutry = "Pol";
+
+
+function Person(name, street){
+    this.name = name;
+    this.street = street;
+
+    this.info = function(){
+        return "My name is " + this.name + " and I live on " + this.street;
+    }
+}
+
+let bobSmith = new Person("Bob", "Polsna");
+let ewaNowak = new Person("Ewa", "Duża")
+
+document.write(bobSmith.info() + "<br />")
+document.write(ewaNowak.info() + "<br />")
